3474ca85d1a7993908d63d865e9282f1c8cc1a6e
Pizzi <alessandro.pizzigalli@epicuramed.it>
2022-01-14 12:52:10 +0100

    Change tasting text

--- src/app/components/tasting/tasting.component.ts
+++ src/app/components/tasting/tasting.component.ts
@@ -1,18 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { fade } from './../../animations';
+import { Component, OnInit } from "@angular/core";
+import { fade } from "./../../animations";
 
 @Component({
-  selector: 'app-tasting',
-  templateUrl: './tasting.component.html',
-  animations: [
-    fade
-  ]
+  selector: "app-tasting",
+  templateUrl: "./tasting.component.html",
+  animations: [fade],
+  styleUrls: ["./tasting.component.css"],
 })
 export class TastingComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
